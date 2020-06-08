31af4a89c8bb3345328c14a30bd231a1b5445245
sly-felipa <sfelipa@cifpfbmoll.eu>
2020-06-08 09:49:34 +0200

    ventana login - posicionando botonoes

--- login.py
+++ login.py
@@ -9,18 +9,35 @@ raiz.title("Login")
 
 # raiz.resizable(0,0)
 
-raiz.config(bg="red")
-
 # raiz.iconbitmap("") ruta
 
-# crear frame
-frameMain = Frame()
+# crear frame, agregar a la raiz
+frameMain = Frame(raiz, width="650",height="350")
 
-# empaquetar frame dentro de la raiz
-frameMain.pack()
+# empaquetar frame dentro de la raiz, se expande x/y
+frameMain.pack(fill="both", expand="true")
 
+# frameMain.config(bg="blue")
 
+labelUsuario = Label(frameMain, text="Usuario", fg="red", font=(12))
+labelUsuario.grid(row=0,column=0, sticky="w", pady=4, padx=4)
 
+campoUsuario = Entry(frameMain)
+campoUsuario.grid(row=0,column=1, padx=10)
+
+labelPassword = Label(frameMain, text="Contraseña",fg="red", font=(12))
+labelPassword.grid(row=1,column=0, sticky="w", pady=4, padx=4)
+
+campoPassword = Entry(frameMain)
+campoPassword.grid(row=1,column=1, padx=10)
+campoPassword.config(show="*")
+
+# botonLogin = Button(frameMain, text="Login")
+botonLogin = Button(frameMain, text="Login", command=raiz.quit)
+botonLogin.grid(row=2,column=0,sticky="news", pady=4,padx=1)
+
+botonRegistro= Button(frameMain, text="Registrarse", command=raiz.quit)
+botonRegistro.grid(row=2,column=1,sticky="news", pady=4, padx=1)
 
 
 
